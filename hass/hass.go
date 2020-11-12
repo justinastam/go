6c57d30d9a7cf8cddae6ccbb0b734ff53d52cb99
Justinas Tamasevicius <justinas.tamasevicius@geniussports.com>
2020-11-12 22:16:45 +0200

    hass sensor history

--- hass/hass.go
+++ hass/hass.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type Hass struct {
@@ -59,34 +60,33 @@ func (h Hass) GetSensorState(sensorId string) SensorState {
 	return sensorState
 }
 
-//func GetSensorHistory(sensorId string, ts time.Time) [][]SensorState {
-//	fmt.Println(ts)
-//	var url = getBaseUrl() + "api/history/period/" + ts.Format("2006-01-02T15:04:05-07:00") + "?filter_entity_id=" + sensorId
-//
-//	resp := doRequest(url)
-//
-//	defer resp.Body.Close()
-//	bodyBytes, err := ioutil.ReadAll(resp.Body)
-//	if err != nil {
-//		log.Fatalln(err)
-//	}
-//
-//	var sensorStates [][]SensorState
-//	json.Unmarshal(bodyBytes, &sensorStates)
-//
-//	if len(sensorStates) == 0 {
-//		return sensorStates
-//	}
-//
-//	// fix remove wrong values
-//	for k, state := range sensorStates[0] {
-//		if !isNumeric(state.State) {
-//			removeStateFromSlice(sensorStates[0], k)
-//		}
-//	}
-//
-//	return sensorStates
-//}
+func (h Hass) GetSensorHistory(sensorId string, ts time.Time) [][]SensorState {
+	var url = h.baseUrl + "api/history/period/" + ts.Format("2006-01-02T15:04:05-07:00") + "?filter_entity_id=" + sensorId
+
+	resp := h.doRequest(url)
+
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var sensorStates [][]SensorState
+	json.Unmarshal(bodyBytes, &sensorStates)
+
+	if len(sensorStates) == 0 {
+		return sensorStates
+	}
+
+	// fix remove wrong values
+	for k, state := range sensorStates[0] {
+		if !isNumeric(state.State) {
+			removeStateFromSlice(sensorStates[0], k)
+		}
+	}
+
+	return sensorStates
+}
 
 func (h Hass) doRequest(url string) *http.Response {
 	req, err := http.NewRequest("GET", url, nil)
