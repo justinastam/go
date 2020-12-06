719af0aba8cdef287df1f7862509946fad04e0d4
Justinas Tamasevicius <justinas.tamasevicius@geniussports.com>
2020-12-06 19:39:54 +0200

    Salus client

---
+++ salus/salus.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+	"time"
+)
+
+type Salus struct {
+	credentials Credentials
+	token string
+	deviceId string
+	deviceValues DeviceValues
+}
+
+type Credentials struct {
+	email string
+	password string
+}
+
+type DeviceValues struct {
+	Temperature float64 		`json:"CH1currentRoomTemp,string"`
+	SetPoint float64 			`json:"CH1currentSetPoint,string"`
+	HeaterStatusString string	`json:"CH1heatOnOffStatus"`
+	HeaterStatus bool
+}
+
+//// @todo delete
+//func main() {
+//	salus := New(getCredentials())
+//	salus.GetTemperature()
+//}
+//
+//// @todo delete
+//func getCredentials() Credentials {
+//	return Credentials{email: "email", password: "pass"}
+//}
+
+func New(credentials Credentials) Salus {
+	salus := Salus{
+		credentials: credentials,
+	}
+
+	return salus
+}
+
+func (s *Salus) GetTemperature() float64 {
+	s.InitDeviceValues()
+
+	return s.deviceValues.Temperature
+}
+
+func (s *Salus) GetSetPoint() float64 {
+	s.InitDeviceValues()
+
+	return s.deviceValues.SetPoint
+}
+
+func (s *Salus) GetIsHeating() bool {
+	s.InitDeviceValues()
+
+	return s.deviceValues.HeaterStatus
+}
+
+func (s *Salus) InitDeviceValues() {
+	// @todo ignore if values are initiated
+
+	s.InitTokenAndDeviceId()
+
+	url := fmt.Sprintf("https://salus-it500.com/public/ajax_device_values.php?devId=%s&token=%s&_=%d", s.deviceId, s.token, time.Now().Unix())
+	resp, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	dv := DeviceValues{}
+	json.Unmarshal(bodyBytes, &dv)
+	dv.HeaterStatus = false
+	if dv.HeaterStatusString == "1" {
+		dv.HeaterStatus = true
+	}
+
+	s.deviceValues = dv
+}
+
+func (s *Salus) InitTokenAndDeviceId() {
+	if s.token != "" && s.deviceId != "" {
+		return
+	}
+
+	client := &http.Client{}
+
+	resp, err := client.Get("https://salus-it500.com/public/login.php?");
+	if err != nil {
+		panic(err)
+	}
+	cookie := strings.Split(resp.Header["Set-Cookie"][0], ";")[0]
+
+	form := url.Values{}
+	form.Add("IDemail", s.credentials.email)
+	form.Add("password", s.credentials.password)
+	form.Add("login", "Login")
+
+	req, err := http.NewRequest("POST", "https://salus-it500.com/public/login.php?", strings.NewReader(form.Encode()))
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Cookie", cookie)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	devIdExists, err := regexp.Match("<input name=\"devId\"", bodyBytes)
+	if err != nil {
+		panic(err)
+	}
+	if !devIdExists {
+		// failed logins return 200 - checking for container with deviceId to indicate success
+		panic("Login failed")
+	}
+
+	req, err = http.NewRequest("GET", "https://salus-it500.com/public/devices.php", nil)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Cookie", cookie)
+
+	resp, err = client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	bodyBytes, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	re := regexp.MustCompile("control\\.php\\?devId=(\\d+)")
+	s.deviceId = string(re.FindSubmatch(bodyBytes)[1])
+
+	re = regexp.MustCompile("<input id=\"token\" name=\"token\" type=\"hidden\" value=\"(\\d+-[a-zA-Z0-9]+)\" />")
+	s.token = string(re.FindSubmatch(bodyBytes)[1])
+}
---
+++ salus/salus_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"os"
+	"strconv"
+	"testing"
+)
+
+func TestGetTemperature(t *testing.T) {
+	salus := New(getCredentials())
+
+	expTemp, err := strconv.ParseFloat(os.Getenv("EXP_TEMP"), 64)
+	if err != nil {
+		t.Errorf("Error: %s", err.Error())
+	}
+
+	salusTemp := salus.GetTemperature()
+	if salusTemp != expTemp {
+		t.Errorf("Temperature incorrect, got: %f, want: %f.", salusTemp, expTemp)
+	}
+}
+
+func TestGetSetPoint(t *testing.T) {
+	salus := New(getCredentials())
+
+	expSP, err := strconv.ParseFloat(os.Getenv("EXP_SET_POINT"), 64)
+	if err != nil {
+		t.Errorf("Error: %s", err.Error())
+	}
+
+	setPoint := salus.GetSetPoint()
+	if setPoint != expSP {
+		t.Errorf("Set point incorrect, got: %f, want: %f.", setPoint, expSP)
+	}
+}
+
+func TestIsHeating(t *testing.T) {
+	salus := New(getCredentials())
+
+	expIsHeating := false
+	if os.Getenv("EXP_IS_HEATING") == "1" {
+		expIsHeating = true
+	} else if os.Getenv("EXP_IS_HEATING") != "0" {
+		panic("Unknown expectation for heating")
+	}
+
+	heating := salus.GetIsHeating()
+	if heating != expIsHeating {
+		t.Errorf("Heater status incorrect")
+	}
+}
+
+func getCredentials() Credentials {
+	return Credentials{email: os.Getenv("SALUS_EMAIL"), password: os.Getenv("SALUS_PASSWORD")}
+}
